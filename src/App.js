d975d544ecf90fdf1f3e6553c6ac235afe73b5fe
arslan <arah09@hotmail.com>
2019-04-20 19:06:56 -0600

    added scaffold for calculator

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import { Button } from './components/Button.jsx';
+import { Input } from './components/Input.jsx'
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ""
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <div className="calc-wrapper">
-          TEST 
+        <Input input={this.state.input}></Input>
+          <div className="row">
+          <Button>7</Button>
+          <Button>8</Button>
+          <Button>9</Button>
+          <Button>/</Button>
+          </div>
+          <div className="row">
+          <Button>4</Button>
+          <Button>5</Button>
+          <Button>6</Button>
+          <Button>X</Button>
+          </div>
+          <div className="row">
+          <Button>1</Button>
+          <Button>2</Button>
+          <Button>3</Button>
+          <Button>+</Button>
+          </div>
+          <div className="row">
+          <Button>.</Button>
+          <Button>0</Button>
+          <Button>=</Button>
+          <Button>-</Button>
+          </div>
        </div>
       </div>
     );
